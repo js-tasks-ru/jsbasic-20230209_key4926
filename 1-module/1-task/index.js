21197db0b2935b74294985b22b04ba7c5a6e6ac7
Dmitrii Keidun <key4926@yandex.ru>
2023-02-16 12:37:29 +0300

    2rd task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,47 @@
 function factorial(n) {
-  // ваш код...
+  let n1 = n;
+  if (n === 0 || n === 1) 
+    return 1; 
+  while (n > 1) { 
+    n--;
+    n1 *= n;
+  }
+  return n1;
 }
+factorial(0);
+
+function factorial(n) {
+  let n1 = n;
+  if (n === 0 || n === 1) 
+    return 1; 
+  while (n > 1) { 
+    n--;
+    n1 *= n;
+  }
+  return n1;
+}
+factorial(1);
+
+function factorial(n) {
+  let n1 = n;
+  if (n === 0 || n === 1) 
+    return 1; 
+  while (n > 1) { 
+    n--;
+    n1 *= n;
+  }
+  return n1;
+}
+factorial(3);
+
+function factorial(n) {
+  let n1 = n;
+  if (n === 0 || n === 1) 
+    return 1; 
+  while (n > 1) { 
+    n--;
+    n1 *= n;
+  }
+  return n1;
+}
+factorial(5);
