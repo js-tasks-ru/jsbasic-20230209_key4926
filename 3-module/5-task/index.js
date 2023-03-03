d34576899cf201e819408b33eda3da97ac0dc0c4
Dmitrii Keidun <key4926@yandex.ru>
2023-03-03 20:17:07 +0300

    3 module 5th task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,11 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(/[ ,]+/);
+  let minNumber = +arr[0];
+  let maxNumber = minNumber;
+  for (let i = 0; i < arr.length; i++) {
+    let a = +arr[i];
+    if (a < minNumber) minNumber = a;
+    if (a > maxNumber) maxNumber = a;
+  }
+  return {min: minNumber, max: maxNumber};
 }
