cf413a89941ec5d818dd53df28265afffb778d68
Dmitrii Keidun <key4926@yandex.ru>
2023-03-05 12:04:16 +0300

    4 module 1st task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  for (let i of friends) {
+    let li = document.createElement('li')
+    li.innerHTML = `${i.firstName} ${i.lastName}`;
+    ul.appendChild(li);
+  }
+  return ul;
 }
