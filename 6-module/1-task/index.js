86fa412364366315514054738d16d15dc2083ea8
Dmitrii Keidun <key4926@yandex.ru>
2023-03-14 14:51:02 +0300

    6-module, 1st task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,32 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = this.render(rows);
   }
+  render (rows) {
+		let button = '<button>X</button>';
+		let table = document.createElement('table');
+		let tbody = document.createElement('tbody');
+        for (let row of rows) {
+	        let tr = document.createElement('tr');
+
+	        for (let tds in row) {
+		        let td = document.createElement('td');
+		        td.innerHTML = row[tds];
+				tr.appendChild(td);
+			}
+
+			let tdButton = document.createElement('td');
+            tr.appendChild(tdButton);
+		    tdButton.insertAdjacentHTML('beforeEnd', button);
+
+		    tr.lastChild.addEventListener('click', function (event) {
+				let elemParent = event.target.parentElement;
+			    elemParent.parentElement.remove();
+		    });
+            tbody.appendChild(tr);
+			table.appendChild(tbody);
+		}
+		return table;
+    }
 }
